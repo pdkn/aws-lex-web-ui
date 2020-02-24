54ceaa1c4f7205592501e04edfb056875c14e4cf
Paddy Keane <Paddy.Keane@akqa.com>
2020-02-24 16:14:06 +0000

    [~] WORKING ON. a11y role attributes

--- lex-web-ui/src/components/ToolbarContainer.vue
+++ lex-web-ui/src/components/ToolbarContainer.vue
@@ -5,6 +5,7 @@
     dark
     fixed
     v-bind:dense="this.$store.state.isRunningEmbedded"
+    role="navigation"
     aria-label="Toolbar with sound FX mute button, minimise chat window button and option chat back a step button"
   >
     <img v-if="toolbarLogo" v-bind:src="toolbarLogo" alt="logo" aria-hidden="true"/>
